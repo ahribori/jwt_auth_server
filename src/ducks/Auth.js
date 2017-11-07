437b5ce731b9f3aecd77b7d999c0f6406f852133
ahribori <ahribori@gmail.com>
2017-11-07 21:03:21 +0900

    - Token Store, Token Verify, Get User 구현

--- src/ducks/Auth.js
+++ src/ducks/Auth.js
@@ -3,8 +3,10 @@ import { fromJS } from 'immutable';
 import helper from './helpers/thunkHelper';
 
 // Action types
-const LOGIN = helper.createThunkTypes('user/LOGIN');
-const JOIN = helper.createThunkTypes('user/JOIN');
+const LOGIN = helper.createThunkTypes('auth/LOGIN');
+const JOIN = helper.createThunkTypes('auth/JOIN');
+const VERIFY = helper.createThunkTypes('auth/VERIFY');
+const GET_USER = helper.createThunkTypes('auth/GET_USER');
 
 // Action creators
 export const login = (username, password) => {
@@ -37,10 +39,32 @@ export const join = (username, password, nickname, email) => {
 	})();
 };
 
+export const verify = token => {
+    return helper.createThunk(VERIFY.DEFAULT, {
+        url: '/api/auth/verify',
+        method: 'get',
+        headers: {
+            authorization: token,
+        }
+    })();
+};
+
+export const getUser = (_id, token) => {
+    return helper.createThunk(GET_USER.DEFAULT, {
+        url: '/api/user/' + _id,
+        method: 'get',
+        headers: {
+            authorization: token,
+        }
+    })();
+};
+
 // Initial state
 const initialState = fromJS({
     join: null,
     login: null,
+    verify: null,
+    user: null,
 });
 
 // Reducer
@@ -64,6 +88,26 @@ export default handleActions({
     },
     [JOIN.FAILURE]: (state, action) => {
         return state.set('join', action.payload);
+    },
+
+    [VERIFY.REQUEST]: (state, action) => {
+        return state;
+    },
+    [VERIFY.SUCCESS]: (state, action) => {
+        return state.set('verify', action.payload);
+    },
+    [VERIFY.FAILURE]: (state, action) => {
+        return state.set('verify', action.payload);
+    },
+
+    [GET_USER.REQUEST]: (state, action) => {
+        return state;
+    },
+    [GET_USER.SUCCESS]: (state, action) => {
+        return state.set('user', action.payload);
+    },
+    [GET_USER.FAILURE]: (state, action) => {
+        return state.set('user', action.payload);
     },
 
 }, initialState);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,127 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as auth from '../ducks/Auth';
+import cookie from 'browser-cookies';
 
 class Login extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            isLoggedIn: false,
             username: '',
             password: '',
         };
     }
 
-    handleLoginRequest = () => {
-        this.props.loginRequest(
-            this.state.username,
-            this.state.password,
-        ).then(() => {
-            const success = this.props.login.success;
-            const token = this.props.login.response.data;
-            if (success) {
-                this.setToken(token);
-            }
+    login = async (username, password) => {
+        // request login
+        await this.props.loginRequest(username, password);
+        // then
+        const success = this.props.login.success;
+        const token = this.props.login.response.data;
+        if (success) {
+            this.setToken(token);
+            this.setState({
+                isLoggedIn: true,
+            })
+        } else {
+            console.error(this.props.login.response.data.message);
+        }
+    };
+
+    logout = () => {
+        this.clearToken();
+        this.setState({
+            isLoggedIn: false,
         })
     };
 
     setToken = (token) => {
-        document.cookie = `access_token=${token};`;
+        cookie.set('access_token', token);
         if (window.localStorage) {
             window.localStorage.setItem('access_token', token);
         }
     };
 
     getToken = () => {
-
+        let token = null;
+        if (window.localStorage) {
+            token = window.localStorage.getItem('access_token');
+        }
+        if (!token) {
+            token = cookie.get('access_token');
+        }
+        return token;
     };
 
     clearToken = () => {
-        console.log(document.cookie);
+        if (window.localStorage) {
+            window.localStorage.removeItem('access_token');
+        }
+        cookie.erase('access_token');
     };
 
-    componentDidMount() {
-        this.clearToken();
+    verifyToken = async token => {
+        await this.props.verifyRequest(token);
+        return this.props.verify;
+    };
+
+    getUser = async (_id, token) => {
+        await this.props.getUserRequest(_id, token);
+        return this.props.user.response.data;
+    };
+
+    isLogin = async () => {
+        const token = this.getToken();
+        if (!token) {
+            return false;
+        } else {
+            const verify = await this.verifyToken(token);
+            return verify.success;
+        }
+    };
+
+    async componentDidMount() {
+        const token = this.getToken();
+        if (token) {
+            this.setState({
+                isLoggedIn: true,
+            })
+        }
+        const isLogin = await this.isLogin();
+        if (isLogin) {
+            this.setState({
+                isLoggedIn: true,
+            })
+        } else {
+            this.setState({
+                isLoggedIn: false,
+            })
+        }
     }
 
     handleChange = e => {
         switch (e.target.name) {
             case 'username':
-                return this.setState({ username: e.target.value });
+                return this.setState({username: e.target.value});
             case 'password':
-                return this.setState({ password: e.target.value });
+                return this.setState({password: e.target.value});
             default:
         }
     };
 
     handleSubmit = e => {
-        this.handleLoginRequest();
+        if (!this.state.isLoggedIn) {
+            this.login(this.state.username, this.state.password);
+        } else {
+            this.logout();
+        }
     };
 
     handleKeyPress = e => {
         if (e.charCode === 13) {
-            this.handleLoginRequest();
+            this.login(this.state.username, this.state.password);
         }
     };
 
@@ -83,15 +144,15 @@ class Login extends React.Component {
                        value={this.state.username}
                        onChange={this.handleChange}
                        onKeyPress={this.handleKeyPress}
-                       style={inputStyle} />
+                       style={inputStyle}/>
                 <label>password</label>
                 <input type="password"
                        name="password"
                        value={this.state.password}
                        onChange={this.handleChange}
                        onKeyPress={this.handleKeyPress}
-                       style={inputStyle} />
-                <button onClick={this.handleSubmit}>Login</button>
+                       style={inputStyle}/>
+                <button onClick={this.handleSubmit}>{this.state.isLoggedIn ? 'Logout' : 'Login'}</button>
             </div>
         );
     };
@@ -103,7 +164,9 @@ class Login extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        login: state.auth.get('login')
+        login: state.auth.get('login'),
+        verify: state.auth.get('verify'),
+        user: state.auth.get('user'),
     }
 };
 
@@ -111,7 +174,13 @@ const mapDispatchToProps = (dispatch) => {
     return {
         loginRequest: (username, password) => {
             return dispatch(auth.login(username, password))
-        }
+        },
+        verifyRequest: token => {
+            return dispatch(auth.verify(token))
+        },
+        getUserRequest: (_id, token) => {
+            return dispatch(auth.getUser(_id, token))
+        },
     }
 };
 
