43ba24dd6c5de8f6d928a10e6bad60bd1afa0363
ahribori <ahribori@gmail.com>
2017-11-11 12:01:15 +0900

    - App index에 Material-ui 테마 정의

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import registerServiceWorker from './registerServiceWorker';
 
 /**
@@ -15,12 +14,27 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './ducks';
 
+
+/**
+ *  Material-UI
+ */
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import * as color from 'material-ui/styles/colors';
+
+
 const middlewares = [thunk];
 const store = createStore(reducers, compose(applyMiddleware(...middlewares)));
 
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: color.pink500
+    }
+});
+
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiTheme}>
             <App/>
         </MuiThemeProvider>
     </Provider>,
