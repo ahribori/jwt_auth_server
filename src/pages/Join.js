c1fbca30538573db37acd8c521071db98c404eb6
ahribori <ahribori@gmail.com>
2017-11-21 23:48:52 +0900

    - Join 화면에 Material UI 적용

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as auth from '../ducks/Auth';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardText, CardTitle} from 'material-ui/Card';
 
 class Join extends React.Component {
 
@@ -9,6 +12,7 @@ class Join extends React.Component {
         this.state = {
             username: '',
             password: '',
+            password_confirm: '',
             nickname: '',
             email: '',
         };
@@ -17,13 +21,15 @@ class Join extends React.Component {
     handleChange = e => {
         switch (e.target.name) {
             case 'username':
-                return this.setState({ username: e.target.value });
+                return this.setState({username: e.target.value});
             case 'password':
-                return this.setState({ password: e.target.value });
+                return this.setState({password: e.target.value});
+            case 'password_confirm':
+                return this.setState({password_confirm: e.target.value});
             case 'nickname':
-                return this.setState({ nickname: e.target.value });
+                return this.setState({nickname: e.target.value});
             case 'email':
-                return this.setState({ email: e.target.value });
+                return this.setState({email: e.target.value});
             default:
         }
     };
@@ -39,31 +45,75 @@ class Join extends React.Component {
 
     _renderJoinForm = () => {
         const containerStyle = {
-            width: '200px',
-            margin: '0 auto'
+            width: '400px',
+            margin: '0 auto',
+            padding: '48px 40px 36px 40px'
         };
 
         const inputStyle = {
             display: 'block',
-            margin: '0.5rem'
+            margin: '0 auto'
+        };
+
+        const buttonStyle = {
+            marginTop: '2rem'
         };
 
         return (
-            <div style={containerStyle}>
-                <label>username</label>
-                <input type="text" name="username" value={this.state.username} onChange={this.handleChange}
-                       style={inputStyle} />
-                <label>password</label>
-                <input type="password" name="password" value={this.state.password} onChange={this.handleChange}
-                       style={inputStyle} />
-                <label>nickname</label>
-                <input type="text" name="nickname" value={this.state.nickname} onChange={this.handleChange}
-                       style={inputStyle} />
-                <label>email</label>
-                <input type="email" name="email" value={this.state.email} onChange={this.handleChange}
-                       style={inputStyle} />
-                <button onClick={this.handleSubmit}>Join</button>
-            </div>
+            <Card style={containerStyle} className="join container">
+                <CardTitle title="계정 만들기" subtitle="아리보리 계정 만들기"/>
+                <CardText>
+                    <TextField
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                        floatingLabelText="계정"
+                        style={inputStyle}
+                    />
+                    <TextField
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        floatingLabelText="패스워드"
+                        style={inputStyle}
+                    />
+                    <TextField
+                        type="password"
+                        name="password_confirm"
+                        value={this.state.password_confirm}
+                        onChange={this.handleChange}
+                        floatingLabelText="패스워드 확인"
+                        style={inputStyle}
+                    />
+                    <TextField
+                        type="text"
+                        name="nickname"
+                        value={this.state.nickname}
+                        onChange={this.handleChange}
+                        floatingLabelText="닉네임"
+                        style={inputStyle}
+                    />
+                    <TextField
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        floatingLabelText="이메일"
+                        style={inputStyle}
+                    />
+                </CardText>
+                <CardActions>
+                    <RaisedButton
+                        onClick={this.handleSubmit}
+                        fullWidth
+                        label='계정 만들기'
+                        primary
+                        style={buttonStyle}
+                    />
+                </CardActions>
+            </Card>
         );
     };
 
