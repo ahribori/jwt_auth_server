818e5e7df5aac2c344fe1f1ca85328e83ee5cb8a
ahribori <ahribori@gmail.com>
2017-12-07 00:19:37 +0900

    - Application Model 추가

---
+++ server/mongodb/models/application.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const Application = new Schema({
+    user: Schema.Types.ObjectId,
+    name: String,
+    origin: String,
+    callback_url: String,
+    thumbnail_image: String,
+    reg_date: { type: Date, default: Date.now },
+    blocked: { type: Boolean, default: false }
+});
+
+export default mongoose.model('Application', Application);
