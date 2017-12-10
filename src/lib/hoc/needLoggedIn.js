39eb8dcf3c66a1c588830c7e918c63ae0fa334d3
ahribori <ahribori@gmail.com>
2017-12-11 01:26:50 +0900

    needLoggedIn, needAdmin 중복 코드 리팩토링

--- src/lib/hoc/needLoggedIn.js
+++ src/lib/hoc/needLoggedIn.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import cookie from 'browser-cookies';
 import { Redirect } from 'react-router-dom';
 import CircularProgress from 'material-ui/CircularProgress';
 import {
     pinkA200 as loadingProgressColor,
     white as loadingFontColor,
 } from 'material-ui/styles/colors';
-import * as auth from '../../ducks/Auth';
 import FullScreenNotification from '../../templates/FullScreenNotification';
+import withAuth from './withAuth';
 
-const mapStateToProps = (state) => {
-    return {
-        login: state.auth.get('login'),
-        verify: state.auth.get('verify'),
-        user: state.auth.get('user'),
-    };
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        loginRequest: (username, password) => dispatch(auth.login(username, password)),
-        verifyRequest: token => dispatch(auth.verify(token)),
-        getUserRequest: (_id, token) => dispatch(auth.getUser(_id, token)),
-    };
-};
-
-export default WrappedComponent => connect(mapStateToProps, mapDispatchToProps)(class needLoggedIn extends React.Component {
+export default WrappedComponent => withAuth(class needLoggedIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -40,11 +22,11 @@ export default WrappedComponent => connect(mapStateToProps, mapDispatchToProps)(
     }
 
     async componentDidMount() {
-        const isLoggedIn = await this.isLoggedIn();
-        const token = this.getToken();
+        const isLoggedIn = await this.props.isLoggedIn();
+        const token = this.props.getToken();
         if (isLoggedIn) {
             const { _id } = this.props.verify.response.data;
-            const user = await this.getUser(_id, token);
+            const user = await this.props.getUser(_id, token);
             this.setState({
                 pending: false,
                 isLoggedIn: true,
@@ -60,61 +42,14 @@ export default WrappedComponent => connect(mapStateToProps, mapDispatchToProps)(
         }
     }
 
-    getToken = () => {
-        let token = null;
-        if (window.localStorage) {
-            token = window.localStorage.getItem('access_token');
-        }
-        if (!token) {
-            token = cookie.get('access_token');
-        }
-        return token;
-    };
-
-    getUser = async (_id, token) => {
-        await this.props.getUserRequest(_id, token);
-        return this.props.user.response.data;
-    };
-
-    verifyToken = async (token) => {
-        await this.props.verifyRequest(token);
-        return this.props.verify;
-    };
-
-    isLoggedIn = async () => {
-        const token = this.getToken();
-        if (!token) {
-            return false;
-        }
-        const verify = await this.verifyToken(token);
-        return verify.success;
-    };
-
-    clearToken = () => {
-        if (window.localStorage) {
-            window.localStorage.removeItem('access_token');
-        }
-        cookie.erase('access_token');
-    };
-
     logout = () => {
-        this.clearToken();
+        this.props.clearToken();
         this.setState({
             isLoggedIn: false,
         });
     };
 
     render() {
-        const {
-            login,
-            verify,
-            user,
-            loginRequest,
-            verifyRequest,
-            getUserRequest,
-            ...props
-        } = this.props;
-
         if (!this.state.pending) {
             if (!this.state.isLoggedIn) {
                 return <Redirect to="/login" />;
@@ -123,9 +58,12 @@ export default WrappedComponent => connect(mapStateToProps, mapDispatchToProps)(
                 return (
                     <FullScreenNotification>
                         <h2>사용자를 찾을 수 없습니다</h2>
-                        <FullScreenNotification.Link text="로그인 하러 가기" onClick={() => {
-                            this.logout();
-                        }} />
+                        <FullScreenNotification.Link
+                            text="로그인 하러 가기"
+                            onClick={() => {
+                                this.logout();
+                            }}
+                        />
                     </FullScreenNotification>
                 );
             }
@@ -139,7 +77,7 @@ export default WrappedComponent => connect(mapStateToProps, mapDispatchToProps)(
             );
         }
         return (
-            <WrappedComponent logout={this.logout} {...this.state} {...props} />
+            <WrappedComponent {...this.state} {...this.props} logout={this.logout} />
         );
     }
 });
