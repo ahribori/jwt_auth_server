f0bb31239416ed1607f580dc4241b863ced73615
Hyun Seung Jeong <ahribori@gmail.com>
2017-12-26 17:22:19 +0900

    회원 정보 수정 패널 작업중...

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ModifyIcon from 'material-ui/svg-icons/content/create';
+import RemoveIcon from 'material-ui/svg-icons/content/remove';
 import 'ag-grid/src/styles/ag-grid.scss';
 import 'ag-grid/src/styles/ag-theme-material.scss';
 
@@ -11,18 +14,74 @@ import '../../style/AgGrid.scss';
 import { Loading } from '../../templates';
 import LevelRenderer from './components/LevelRenderer';
 import ProfileImageRenderer from './components/ProfileImageRenderer';
+import ModifyModal from './components/ModifyModal';
 import needAdmin from '../../lib/hoc/needAdmin';
 
 @needAdmin
 class Users extends React.Component {
+    state = {
+        modifyModalOpen: false,
+        selectedRow: [],
+    };
+
     async componentDidMount() {
         await this.props.getUserListRequest(this.props.auth.token);
     }
+
+
+    onGridReady = (params) => {
+        this.api = params.api;
+        this.columnApi = params.columnApi;
+    };
+
+    onSelectionChanged = () => {
+        this.setState({
+            selectedRow: this.api.getSelectedRows(),
+        });
+    };
+
+    handleModifyButtonClick = () => {
+        this.openModifyModal();
+    };
+
+    handleRemoveButtonClick = () => {
+    };
+
+    openModifyModal = () => {
+        this.setState({
+            modifyModalOpen: true,
+        });
+    };
+
+    closeModifyModal = () => {
+        this.setState({
+            modifyModalOpen: false,
+        });
+    };
+
+    renderModifyButton = () => (this.state.selectedRow.length === 1 ? (
+        <FloatingActionButton
+            className="user-modify-btn"
+            onClick={this.handleModifyButtonClick}
+        >
+            <ModifyIcon />
+        </FloatingActionButton>
+    ) : null);
+
+    renderRemoveButton = () => (this.state.selectedRow.length >= 1 ? (
+        <FloatingActionButton
+            className="user-remove-btn"
+            onClick={this.handleRemoveButtonClick}
+        >
+            <RemoveIcon />
+        </FloatingActionButton>
+    ) : null);
 
     renderGrid = () => {
         const data = [];
         const users = this.props.userList.response.data;
         users.map(user => data.push({
+            _id: user._id,
             profile_image: user.profile_image,
             username: user.username,
             nickname: user.nickname,
@@ -38,69 +97,88 @@ class Users extends React.Component {
             height: 620,
             width: '100%',
         };
-        return (
-            <div style={containerStyle} className="ag-theme-material">
-                <AgGridReact
-                    rowData={data}
-                    rowSelection="multiple"
-                    pagination
-                    paginationPageSize={10}
-                    enableSorting
-                    enableColResize
-                    onGridReady={this.onGridReady}
-                >
 
-                    {/* column definitions */}
-                    <AgGridColumn
-                        field="profile_image"
-                        headerName=""
-                        cellRendererFramework={ProfileImageRenderer}
-                        suppressResize
-                        width={65}
-                    />
-                    <AgGridColumn
-                        field="username"
-                        headerName="계정"
-                        width={130}
-                    />
-                    <AgGridColumn
-                        field="nickname"
-                        headerName="닉네임"
-                        width={130}
-                    />
-                    <AgGridColumn
-                        field="level"
-                        headerName="레벨"
-                        cellRendererFramework={LevelRenderer}
-                        comparator={(r1, r2) => r1.level_details.exp - r2.level_details.exp}
-                        width={150}
-                    />
-                    <AgGridColumn
-                        field="cash"
-                        headerName="캐쉬"
-                        width={120}
-                    />
-                    <AgGridColumn
-                        field="point"
-                        headerName="포인트"
-                        width={120}
-                    />
-                    <AgGridColumn
-                        field="email"
-                        headerName="이메일"
-                    />
-                    <AgGridColumn
-                        field="email_verified"
-                        headerName="이메일 인증"
-                        width={135}
-                    />
-                    <AgGridColumn
-                        field="last_login"
-                        headerName="마지막 로그인"
-                        tooltipField="last_login"
-                        width={250}
-                    />
-                </AgGridReact>
+        return (
+            <div className="users-page">
+                <div className="toolbox">
+                    {this.renderRemoveButton()}
+                    {this.renderModifyButton()}
+                </div>
+                <div style={containerStyle} className="ag-theme-material">
+                    <AgGridReact
+                        rowData={data}
+                        rowSelection="multiple"
+                        rowDeselection
+                        pagination
+                        paginationPageSize={10}
+                        enableSorting
+                        enableColResize
+                        onGridReady={this.onGridReady}
+                        onRowSelected={this.onRowSelected}
+                        onSelectionChanged={this.onSelectionChanged}
+                    >
+
+                        {/* column definitions */}
+                        <AgGridColumn
+                            field="_id"
+                            hide
+                        />
+                        <AgGridColumn
+                            field="profile_image"
+                            headerName=""
+                            cellRendererFramework={ProfileImageRenderer}
+                            suppressResize
+                            width={65}
+                        />
+                        <AgGridColumn
+                            field="username"
+                            headerName="계정"
+                            width={130}
+                        />
+                        <AgGridColumn
+                            field="nickname"
+                            headerName="닉네임"
+                            width={130}
+                        />
+                        <AgGridColumn
+                            field="level"
+                            headerName="레벨"
+                            cellRendererFramework={LevelRenderer}
+                            comparator={(r1, r2) => r1.level_details.exp - r2.level_details.exp}
+                            width={150}
+                        />
+                        <AgGridColumn
+                            field="cash"
+                            headerName="캐쉬"
+                            width={120}
+                        />
+                        <AgGridColumn
+                            field="point"
+                            headerName="포인트"
+                            width={120}
+                        />
+                        <AgGridColumn
+                            field="email"
+                            headerName="이메일"
+                        />
+                        <AgGridColumn
+                            field="email_verified"
+                            headerName="이메일 인증"
+                            width={135}
+                        />
+                        <AgGridColumn
+                            field="last_login"
+                            headerName="마지막 로그인"
+                            tooltipField="last_login"
+                            width={250}
+                        />
+                    </AgGridReact>
+                </div>
+                <ModifyModal
+                    open={this.state.modifyModalOpen}
+                    handleClose={this.closeModifyModal}
+                    data={this.state.selectedRow[0]}
+                />
             </div>
         );
     };
---
+++ src/pages/Users/components/ModifyModal.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import Loading from '../../../templates/Loading';
+
+class ModifyModal extends React.Component {
+    static propTypes = {
+        open: PropTypes.bool,
+        data: PropTypes.object,
+        handleClose: PropTypes.func,
+        handleRequest: PropTypes.func,
+    };
+
+    static defaultProps = {
+        open: false,
+        data: null,
+        handleClose: () => {
+        },
+        handleRequest: () => {
+        },
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            pending: false,
+            password: '',
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const data = { ...nextProps.data };
+        this.setState({
+            ...data,
+            exp: data.level.level_details.exp,
+        });
+    }
+
+    submit = async () => {
+        if (this.validateFields()) {
+            this.setState({ pending: true });
+            const response = await this.props.handleRequest(
+                this.props.data._id,
+                this.state.name,
+                this.state.origin,
+                this.props.auth.token,
+            );
+            this.setState({ pending: false });
+
+            if (response.success) {
+                this.setState({
+                    name: '',
+                    origin: '',
+                });
+                this.props.handleClose();
+            } else {
+                console.info(response.response.status);
+            }
+            return response;
+        }
+        return null;
+    };
+
+    validateFields = () => {
+    };
+
+    handleChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    renderFields = () => (
+        <div>
+            <TextField
+                fullWidth
+                floatingLabelText="닉네임"
+                hintText="닉네임을 입력하세요"
+                name="nickname"
+                value={this.state.nickname}
+                onChange={this.handleChange}
+            />
+            <TextField
+                fullWidth
+                type="password"
+                floatingLabelText="패스워드"
+                hintText="패스워드를 입력하세요"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+            />
+            <TextField
+                fullWidth
+                floatingLabelText="이메일"
+                hintText="이메일을 입력하세요"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+            />
+            <TextField
+                fullWidth
+                floatingLabelText="캐쉬"
+                hintText="캐쉬를 입력하세요"
+                name="cash"
+                value={this.state.cash}
+                onChange={this.handleChange}
+            />
+            <TextField
+                fullWidth
+                floatingLabelText="포인트"
+                hintText="포인트를 입력하세요"
+                name="point"
+                value={this.state.point}
+                onChange={this.handleChange}
+            />
+            <TextField
+                fullWidth
+                floatingLabelText="경험치"
+                hintText="경험치를 입력하세요"
+                name="exp"
+                value={this.state.exp}
+                onChange={this.handleChange}
+            />
+        </div>
+    );
+
+    renderActions = () => [
+        <FlatButton
+            label="닫기"
+            onClick={this.props.handleClose}
+        />,
+        <FlatButton
+            label="적용"
+            primary
+            onClick={this.submit}
+        />,
+    ];
+
+    renderModifyModal = () => (
+        <Dialog
+            modal
+            title="사용자 계정 수정"
+            actions={this.renderActions()}
+            open={this.props.open}
+            contentClassName="dialog"
+        >
+            {this.state.pending ? <Loading /> : this.renderFields()}
+        </Dialog>
+    );
+
+    render() {
+        return this.state._id ? this.renderModifyModal() : null;
+    }
+}
+
+export default ModifyModal;
