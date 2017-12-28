de1125817908d76a3b9fc40b7a532278c2c2a852
Hyun Seung Jeong <ahribori@gmail.com>
2017-12-28 14:30:19 +0900

    중복 이미지 삭제

--- src/pages/Users/components/AccountRenderer.js
+++ src/pages/Users/components/AccountRenderer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Avatar from 'material-ui/Avatar';
 
-import kakaoImage from './images/kakao_sqare_icon.png';
-import facebookImage from './images/facebook_sqare_icon.png';
-import googleImage from './images/google_sqare_icon.png';
+import kakaoLogo from '../../Login/components/SocialLogin/styles/kakao_logo.png';
+import facebookLogo from '../../Login/components/SocialLogin/styles/facebook_logo.png';
+import googleLogo from '../../Login/components/SocialLogin/styles/google_logo.png';
 
 class AccountRenderer extends React.Component {
     static propTypes = {};
@@ -19,17 +19,17 @@ class AccountRenderer extends React.Component {
     render() {
         const { username, account_type } = this.props.value;
         if (account_type) {
-            const imageType = {
-                kakao: kakaoImage,
-                facebook: facebookImage,
-                google: googleImage,
+            const logoType = {
+                kakao: kakaoLogo,
+                facebook: facebookLogo,
+                google: googleLogo,
             };
             return (
                 <div style={{
                     marginTop: 3,
                 }}
                 >
-                    <Avatar size={40} src={imageType[account_type]} />
+                    <Avatar size={40} src={logoType[account_type]} style={{ backgroundColor: 'transparent' }} />
                 </div>
             );
         }
